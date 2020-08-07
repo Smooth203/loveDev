eeb3e43e773c68cd450b3b86b3e42f15ed96b5ec
James <jamesjwaggett@gmail.com>
2020-08-07 21:12:28 +0100

    owe3

--- OWE3/world.lua
+++ OWE3/world.lua
@@ -2,46 +2,51 @@ World = {
 	load = function(self)
 			self.x = 0
 			self.y = 0
-			self.w = 100
-			self.h = 100
+			self.w = 1000
+			self.h = 1000
 			self.tileSize = 30
 			self.tileDisplayW = sw/30
 			self.tileDisplayH = sh/30
 			self.world = {}
-			for x = 0, self.w do
+			self.tileCount = 0
+			for x = 0, self.w-1 do
 				self.world[x] = {}
-				for y = 0, self.h do
-					self.world[x][y] = 0
+				for y = 0, self.h-1 do
+					self.tileCount = self.tileCount + 1
+					self.world[x][y] = {
+						id = self.tileCount,
+						value = 0
+					}
 				end
 			end
-			World:updateTiles()
 		end,
 
 	draw = function(self)
-			for i, tile in ipairs(self.loadedTiles) do
-				love.graphics.print(tile.id, tile.x*self.tileSize, tile.y*self.tileSize)
+			for x = 0, self.w-1 do
+				for y = 0, self.h-1 do
+					--love.graphics.rectangle('line', x*20, y*20, 20, 20)
+				end
 			end
 		end,
 
 	update = function (self, dt)
-		--self.x = self.x + 1
-		World:updateTiles()
+		--self.x = self.x - 1
 	end,
 
-	updateTiles = function(self)
-		self.loadedTiles = {}
-		local count = 0
-		for x = 0, self.tileDisplayW do
-			for y = 0, self.tileDisplayH do
-				count = count + 1
+	-- loadTiles = function(self)
+	-- 	self.loadedTiles = {}
+	-- 	local count = 0
+	-- 	for x = 0, self.tileDisplayW do
+	-- 		for y = 0, self.tileDisplayH do
+	-- 			count = count + 1
 
-				local tile = {}
-				tile.id = count
-				tile.x = x
-				tile.y = y
+	-- 			local tile = {}
+	-- 			tile.id = count
+	-- 			tile.x = x*self.tileSize
+	-- 			tile.y = y*self.tileSize
 
-				table.insert(self.loadedTiles, tile)
-			end
-		end
-	end
+	-- 			table.insert(self.loadedTiles, tile)
+	-- 		end
+	-- 	end
+	-- end
 }
